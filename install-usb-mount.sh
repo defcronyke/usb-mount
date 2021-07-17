549c58516d32884b4cdbd1bb67e59176b8744824
Jeremy Carter <jeremy@jeremycarter.ca>
2021-07-17 06:39:29 -0400

    Initial commit

---
+++ install-usb-mount.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+ln -sf /media $HOME/mnt 2>/dev/null
+sudo cp -f usb-mount@.service /etc/systemd/system/
+sudo cp -f 99-usb-mount.rules /etc/udev/rules.d/
+sudo udevadm control --reload-rules && sudo udevadm trigger
+sudo systemctl daemon-reload
+
